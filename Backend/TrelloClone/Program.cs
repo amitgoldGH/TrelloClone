27c297355ca6db59bcc4f1316c5473b2f7d09a72
Amit <amit.goldshtein2@gmail.com>
2023-03-17 00:09:51 +0200

    Added automapper and boardrepository to program.cs

--- Backend/TrelloClone/Program.cs
+++ Backend/TrelloClone/Program.cs
@@ -11,7 +11,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 //builder.Services.AddTransient<Seed>();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IBoardRepository, BoardRepository>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
