4a498a5e9bbf066dd62ef197b3b48a0938b08566
Amit <amit.goldshtein2@gmail.com>
2023-03-17 00:07:25 +0200

    Created board controller with get all boards

---
+++ Backend/TrelloClone/Controllers/BoardController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TrelloClone.DTO;
+using TrelloClone.Interfaces;
+using TrelloClone.Models;
+
+namespace TrelloClone.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BoardController : Controller
+    {
+        private readonly IBoardRepository _boardRepository;
+
+        public BoardController(IBoardRepository boardRepository)
+        {
+            _boardRepository = boardRepository;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<KanbanBoardDTO>))]
+        public IActionResult GetBoards()
+        {
+            var boards = _boardRepository.GetAllBoards();
+            return Ok(boards);
+        }
+    }
+}
