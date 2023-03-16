0924eab44f9ee31b2ede3e4b3e6072d8eadb3cc6
Amit <amit.goldshtein2@gmail.com>
2023-03-17 00:08:39 +0200

    Added mapping profile for ProjectTo to mape from model -> dto for memberships

---
+++ Backend/TrelloClone/MapHelper/MappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using TrelloClone.DTO;
+using TrelloClone.Models;
+
+namespace TrelloClone.MapHelper
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<User, UserDTO>();
+
+            CreateMap<KanbanBoard, KanbanBoardDTO>();
+
+            CreateMap<Membership, UserMembershipDTO>().ForMember(dest => dest.BoardTitle, opt => opt.MapFrom(src => src.KanbanBoard.Title));
+
+            CreateMap<Membership, BoardMembershipDTO>().ForMember(dest => dest.Username, opt => opt.MapFrom(src => src.UserId));
+        }
+    }
+}
