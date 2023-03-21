50d0cf62ddf647e826f12f14f6afe0dab24712a4
Amit <amit.goldshtein2@gmail.com>
2023-03-21 08:44:45 +0200

    Added projectto mapping for differently named variable of kanbanboard -> kanbanboardDTO

--- Backend/TrelloClone/MapHelper/MappingProfile.cs
+++ Backend/TrelloClone/MapHelper/MappingProfile.cs
@@ -10,7 +10,7 @@ namespace TrelloClone.MapHelper
         {
             CreateMap<User, UserDTO>();
 
-            CreateMap<KanbanBoard, KanbanBoardDTO>();
+            CreateMap<KanbanBoard, KanbanBoardDTO>().ForMember(dest => dest.Members, opt => opt.MapFrom(src => src.Memberships));
 
             CreateMap<Membership, UserMembershipDTO>().ForMember(dest => dest.BoardTitle, opt => opt.MapFrom(src => src.KanbanBoard.Title));
 
