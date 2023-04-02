d10f100c9edae22116e26eefe47063fc3131ccc6
Amit <amit.goldshtein2@gmail.com>
2023-04-02 20:20:38 +0300

    Created a request initiator DTO used to asses access via JWT token

---
+++ Backend/TrelloClone/DTO/Display/RequestInitiatorDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace TrelloClone.DTO.Display
+{
+    public class RequestInitiatorDTO
+    {
+        public string Username { get; set; }
+
+        public string Role { get; set; }
+
+        public int[] BoardMemberships { get; set; }
+    }
+}
