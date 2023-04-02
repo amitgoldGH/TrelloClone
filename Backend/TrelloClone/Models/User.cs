6862dd77181cf0d876ae2172404cda203a1675e7
Amit <amit.goldshtein2@gmail.com>
2023-04-02 20:10:34 +0300

    Added Role to user model for authorization of specific actions

--- Backend/TrelloClone/Models/User.cs
+++ Backend/TrelloClone/Models/User.cs
@@ -1,15 +1,19 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
 
 namespace TrelloClone.Models
 {
     public class User
     {
+
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public string Username { get; set; } // Private key
 
         public string Password { get; set; }
+
+        public string Role { get; set; }
 
         public ICollection<Membership> Memberships { get; set; }
 
