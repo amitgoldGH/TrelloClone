01c9c12a28cab82cb156111bf5bd335cf87af561
Amit <amit.goldshtein2@gmail.com>
2023-04-03 17:49:10 +0300

    Added unauthorized exception and messages

---
+++ Backend/TrelloClone/Exceptions/MiscExceptions.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.Serialization;
+
+namespace TrelloClone.Exceptions
+{
+    public class UnauthorizedAction : Exception
+    {
+        public UnauthorizedAction() { }
+
+        public UnauthorizedAction(string? message) : base(message)
+        {
+        }
+
+        public UnauthorizedAction(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        protected UnauthorizedAction(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
