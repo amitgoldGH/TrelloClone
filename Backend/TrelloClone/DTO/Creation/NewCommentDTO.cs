9acfd84efeac5c9194096d16abcb5972d3c69721
Amit <amit.goldshtein2@gmail.com>
2023-04-03 20:27:04 +0300

    commented out unused variables.

--- Backend/TrelloClone/DTO/Creation/NewCommentDTO.cs
+++ Backend/TrelloClone/DTO/Creation/NewCommentDTO.cs
@@ -4,9 +4,9 @@
     {
         public string Text { get; set; }
 
-        public string AuthorName { get; set; }
+        //public string AuthorName { get; set; }
 
-        public int CardId { get; set; }
+        //public int CardId { get; set; }
 
     }
 }
